89946c4382dc13df3a4a31d43b985127cec7352a
Keillion <735699921@qq.com>
2021-07-08 15:46:11 +0800

    express self host

---
+++ dbrjs-express-wasm-self-host/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs-extra');
+const https = require('https');
+const path = require('path');
+
+const app = express();
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+let httpsServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'pem/ryans-key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'pem/ryans-cert.pem'))
+}, app);
+
+let httpsPort = 4443;
+httpsServer.listen(httpsPort, () => console.log('Page is available in https://localhost:'+httpsPort+'/'));
