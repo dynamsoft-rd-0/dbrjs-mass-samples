a54ffbb1218b941665f2dc7e2e36c49f47bb0682
Keillion <735699921@qq.com>
2022-01-11 16:26:51 +0800

    Update app.js

--- nodejs-memory-leak/app.js
+++ nodejs-memory-leak/app.js
@@ -1,3 +1,4 @@
+// node ./app.js
 const express = require('express');
 const fs = require('fs');
 const http = require('http');
@@ -8,36 +9,44 @@ const DBR = require('keillion-dynamsoft-javascript-barcode');
 
 //expire in 2022-02-10
 DBR.BarcodeReader.productKeys = 't0068MgAAACpEXBhcD4bWoh/rX54MC4LbThO29LVsJM884E6DVnmZTyzI1MY9nhK/zu1bujgxTzqpqOTP3wQr9vOn7JGwp0I=';
+(async()=>{
+    // create a temp dbr instance, so we can init license
+    {
+        const tempReaderr = await DBR.BarcodeReader.createInstance();
+        await tempReaderr.destroy();
+    }
 
-const app = express();
+    const app = express();
 
-const upload = multer({storage: multer.memoryStorage()});
-
-app.post('/decode', upload.any(), async(req, res) => {
-    let reader;
-    let txts = [];
-    try{
-        if(req.files.length){
-            reader = await DBR.BarcodeReader.createInstance();
-            for(let file of req.files){
-                const results = await reader.decode(file.buffer);
-                for(let result of results){
-                    txts.push(result.barcodeText);
+    const upload = multer({storage: multer.memoryStorage()});
+    
+    app.post('/decode', upload.any(), async(req, res) => {
+        let reader;
+        let txts = [];
+        try{
+            if(req.files.length){
+                reader = await DBR.BarcodeReader.createInstance();
+                for(let file of req.files){
+                    const results = await reader.decode(file.buffer);
+                    for(let result of results){
+                        txts.push(result.barcodeText);
+                    }
                 }
             }
+        }finally{
+            if(reader){
+                await reader.destroy();
+            }
         }
-    }finally{
-        if(reader){
-            reader.destroy();
-        }
-    }
-    res.send(txts);
-});
-
-// static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-let httpsServer = http.createServer(app);
-
-let httpsPort = 12380;
-httpsServer.listen(httpsPort, () => console.log('Page is available in http://localhost:'+httpsPort+'/'));
+        res.send(txts);
+    });
+    
+    // static files
+    app.use(express.static(path.join(__dirname, 'public')));
+    
+    let httpsServer = http.createServer(app);
+    
+    let httpsPort = 12380;
+    httpsServer.listen(httpsPort, () => console.log('Page is available in http://localhost:'+httpsPort+'/'));
+    
+})();
