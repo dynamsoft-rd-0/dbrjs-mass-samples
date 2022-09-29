326b0f788aa453054d2908cf932f1bb205ee8128
Keillion <735699921@qq.com>
2022-09-29 18:03:53 +0800

    dbrjs react hooks

---
+++ react-hooks/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoDecoderComponent from './VideoDecoderComponent';
+
+function App() {
+  return (
+    <div style={{width:"100vw",height:"100vh"}}>
+      <VideoDecoderComponent></VideoDecoderComponent>
+    </div>
+  );
+}
+
+export default App;
