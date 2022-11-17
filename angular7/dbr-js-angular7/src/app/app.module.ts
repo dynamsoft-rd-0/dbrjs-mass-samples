40891c2e72e7263d258ca23e6402647b1beb1887
Cube <877211593@qq.com>
2022-11-17 18:11:38 +0800

    add

---
+++ angular7/dbr-js-angular7/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { VideoDecodeComponent } from './barcode-scanner/barcode-scanner.component';
+import { ImgDecodeComponent } from './img-decode/img-decode.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloWorldComponent,
+    VideoDecodeComponent,
+    ImgDecodeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
