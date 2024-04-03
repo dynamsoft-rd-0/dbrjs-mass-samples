98285969a26e457a41cec15faeb49c1a8bbde690
Keillion <keillionv@gmail.com>
2024-04-03 20:42:46 +0800

    native way add ui in react

--- auto-turn-on-flash/react/src/App.tsx
+++ auto-turn-on-flash/react/src/App.tsx
@@ -1,17 +1,21 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import './App.css';
 import reactLogo from './assets/logo.svg';
 import VideoCapture from './components/VideoCapture/VideoCapture';
 
 function App() {
-  const [mode, setMode] = useState("video");
+  const [bScan, setBScan] = useState(false);
+  const switchScan = useCallback(()=>{
+    setBScan(!bScan);
+  },[bScan]);
   return (
     <div className='App'>
       <div className='title'>
         <h2 className='title-text'>Auto Turn on Flash</h2>
         <img className='title-logo' src={reactLogo} alt="logo"></img>
       </div>
-      <VideoCapture />
+      <button onClick={switchScan}>{bScan?'stop':'start'} scan</button>
+      {bScan && <VideoCapture />}
     </div>
   );
 }
--- auto-turn-on-flash/react/src/components/VideoCapture/VideoCapture.tsx
+++ auto-turn-on-flash/react/src/components/VideoCapture/VideoCapture.tsx
@@ -1,160 +1,162 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useRef, useState, useCallback, MutableRefObject } from 'react';
 import '../../cvr';
 import { CameraEnhancer, CameraView } from 'dynamsoft-camera-enhancer';
 import { DecodedBarcodesResult } from 'dynamsoft-barcode-reader';
 import { CaptureVisionRouter, CapturedResultReceiver } from 'dynamsoft-capture-vision-router';
 import { MultiFrameResultCrossFilter } from "dynamsoft-utility";
 import { EnumImagePixelFormat } from "dynamsoft-core";
-import "./VideoCapture.css";
+
+const strErrorDistoryed = 'videoCapture component destoryed';
 
 function VideoRecognizer() {
-    const [pRouter, setPRouter] = useState<Promise<CaptureVisionRouter> | null>(null);
-    const [cameraEnhancer, setCameraEnhancer] = useState<CameraEnhancer | null>(null);
-    const [ui, setUI] = useState<HTMLElement | null>(null);
-    const [facingMode, setFacingMode] = useState<'environment' | 'user'>('environment');
-    const [bNotSupportTorch, setBNotSupportTorch] = useState<boolean>(false);
-    const [bTorch, setBTorch] = useState<boolean>(false);
-    const [handleDetectDark, setHandleDetectDark] = useState<number|null>(null);
-  
-    const handleSwitchCamera = useCallback(async () => {
-      console.log('click SwitchCamera');
-      let _facingMode:'environment' | 'user' = facingMode === 'environment' ? 'user' : 'environment';
-      setFacingMode(_facingMode);
-      await cameraEnhancer?.updateVideoSettings({
-        video: {
-          width: { ideal: 1280 },
-          height: { ideal: 720 },
-          facingMode: { ideal: _facingMode }
-        }
-      });
-    },[]);
-  
-    const handleSwitchTorch = useCallback(async () => {
-      console.log('click SwitchTorch');
-      console.log('cameraEnhancer',cameraEnhancer)
-      try {
-        let _bTorch = !bTorch;
-        if (bTorch) {
-            console.log(1);
-          await cameraEnhancer?.turnOffTorch();
-          console.log(2);
-        } else {
-            console.log(3);
-          await cameraEnhancer?.turnOnTorch();
-          console.log(4);
-        }
-        setBTorch(_bTorch);
-      } catch (ex) {
-        console.error(ex);
-        console.warn('torch is not supported.');
-        setBNotSupportTorch(true);
-        (ui!.shadowRoot!.querySelector('.mydbr-switch-torch') as HTMLElement).style.display = 'none';
-      }
-    },[]);
-  
-    const handleResultReceiver = useCallback((result: DecodedBarcodesResult) => {
-      if (result.barcodeResultItems.length > 0) {
-        for (let item of result.barcodeResultItems) {
-          console.log(`${item.formatString}: ${item.text}`);
-        }
-      }
-    },[]);
-  
-    const autoTurnOnFlashWhenDark = useCallback(() => {
 
-      let _handleDetectDark = setInterval(async () => {
-        if (bNotSupportTorch || bTorch || !cameraEnhancer?.isOpen()) {
-          clearInterval(handleDetectDark!);
-          setHandleDetectDark(null);
-          return;
-        }
-  
-        const image = cameraEnhancer?.fetchImage();
-        if (!image || !image.width || !image.height) return;
-  
-        let grayValue = 0;
-        if (image.format === EnumImagePixelFormat.IPF_GRAYSCALED) {
-          grayValue = image.bytes.reduce((sum, byte) => sum + byte, 0) / image.bytes.length;
-        } else if (image.format === EnumImagePixelFormat.IPF_ABGR_8888) {
-          for (let i = 0; i < image.bytes.length; i += 4) {
-            grayValue += image.bytes[i] + image.bytes[i + 1] + image.bytes[i + 2];
-          }
-          grayValue /= image.bytes.length * 3;
-        } else {
-          console.error('Unknown image pixel type');
-          return;
-        }
-  
-        console.log('grayValue: ' + grayValue);
-        if (grayValue < 20) {
-          console.log('Try auto turn on torch');
-          try {
-            await cameraEnhancer?.turnOnTorch();
-            setBTorch(true);
-          } catch (ex) {
+  const rfElContainer:MutableRefObject<HTMLDivElement|null> = useRef(null);
+
+  useEffect(()=>{
+    if(!rfElContainer.current){return;}
+
+    let resolveInit:()=>void;
+    let pInit:Promise<void> = new Promise(r=>{resolveInit=r});
+    let bDestoryed = false;
+
+    let cvr:CaptureVisionRouter;
+    let view:CameraView;
+    let cameraEnhancer:CameraEnhancer;
+
+    let facingMode:'environment'|'user' = 'environment';
+    let bNotSupportTorch = false;
+    let bTorch = false;
+
+    let taskDetectDrak:ReturnType<typeof setInterval>|null;
+    (async()=>{
+      try{
+        cvr = await CaptureVisionRouter.createInstance();
+        view = await CameraView.createInstance('./myUI.html');
+        cameraEnhancer = await CameraEnhancer.createInstance(view);
+        let ui = view.getUIElement();
+        // switch camera
+        ui!.shadowRoot!.querySelector('.mydbr-switch-camera')!.addEventListener('click',async()=>{
+          facingMode = 'environment' === facingMode ? 'user' : 'environment';
+          await cameraEnhancer.updateVideoSettings({video: {width: {ideal: 1280},height: {ideal: 720},facingMode: {ideal: facingMode}}});
+        });
+        // switch torch
+        ui!.shadowRoot!.querySelector('.mydbr-switch-torch')!.addEventListener('click',async()=>{
+          try{
+            if(bTorch){
+                await cameraEnhancer.turnOffTorch();
+            }else{
+                await cameraEnhancer.turnOnTorch();
+            }
+            bTorch = !bTorch;
+          }catch(ex){
             console.error(ex);
             console.warn('torch is not supported.');
-            setBNotSupportTorch(true);
+            bNotSupportTorch = true;
             (ui!.shadowRoot!.querySelector('.mydbr-switch-torch') as HTMLElement).style.display = 'none';
           }
-        }
-      }, 2000);
-      setHandleDetectDark(_handleDetectDark as any as number);
-    }, []);
-  
-    const handleScan = async () => {
-      if (pRouter) return;
-  
-      let _pRouter = CaptureVisionRouter.createInstance();
-      setPRouter(_pRouter);
-      let router = await _pRouter;
-  
-      let view = await CameraView.createInstance('./myUI.html');
-      let _cameraEnhancer = await CameraEnhancer.createInstance(view);
-      setCameraEnhancer(_cameraEnhancer);
-  
-      let newUi = view.getUIElement();
-      newUi.shadowRoot?.querySelector('.mydbr-switch-camera')?.addEventListener('click', handleSwitchCamera);
-      newUi.shadowRoot?.querySelector('.mydbr-switch-torch')?.addEventListener('click', handleSwitchTorch);
-      setUI(newUi);
-      document.body.append(newUi);
-      router.setInput(_cameraEnhancer);
-  
-      router.addResultReceiver({ onDecodedBarcodesReceived: handleResultReceiver });
-  
-      let filter = new MultiFrameResultCrossFilter();
-      filter.enableResultCrossVerification("barcode", true);
-      filter.enableResultDeduplication("barcode", true);
-      await router.addResultFilter(filter);
-  
-      await _cameraEnhancer!.open();
-      await router.startCapturing("ReadSingleBarcode");
+        });
+        
+        // check if conponent still existed
+        if(bDestoryed){ throw Error(strErrorDistoryed); }
+        rfElContainer.current!.append(ui);
+        cvr.setInput(cameraEnhancer);
 
-      autoTurnOnFlashWhenDark();
-    };
+        cvr.addResultReceiver({ onDecodedBarcodesReceived: (result) => {
+          if (result.barcodeResultItems.length > 0) {
+            for (let item of result.barcodeResultItems) {
+              console.log(`${item.formatString}: ${item.text}`);
+            }
+          }
+        }});
 
-    useEffect(()=>{
-      // destroy things
-      return ()=>{(async()=>{
-        if(null != handleDetectDark){ 
-          clearInterval(handleDetectDark); 
-          setHandleDetectDark(null);
+        let filter = new MultiFrameResultCrossFilter();
+        filter.enableResultCrossVerification("barcode", true);
+        filter.enableResultDeduplication("barcode", true);
+        await cvr.addResultFilter(filter);
+
+        if(bDestoryed){ throw Error(strErrorDistoryed); }
+        await cameraEnhancer!.open();
+        if(bDestoryed){ throw Error(strErrorDistoryed); }
+        await cvr.startCapturing("ReadSingleBarcode");
+        if(bDestoryed){ throw Error(strErrorDistoryed); }
+        
+        // detect dark every 2s
+        taskDetectDrak = setInterval(async()=>{
+
+          if(bNotSupportTorch){
+            clearInterval(taskDetectDrak!);
+            taskDetectDrak = null;
+            return;
+          }
+
+          if(bTorch || !cameraEnhancer.isOpen()){ return; }
+
+          let image = cameraEnhancer.fetchImage();
+          if(!image || !image.width || !image.height){ return; }
+
+          let timeStart = Date.now();
+          let grayValue = 0;
+          if(EnumImagePixelFormat.IPF_GRAYSCALED === image.format){
+            for(let i = 0; i < image.bytes.length; ++i){
+              grayValue += image.bytes[i];
+            }
+            grayValue /= image.bytes.length;
+          }else if(EnumImagePixelFormat.IPF_ABGR_8888 === image.format){
+            for(let i = 0; i < image.bytes.length; i+=4){
+              grayValue += image.bytes[i];
+              grayValue += image.bytes[i+1];
+              grayValue += image.bytes[i+2];
+            }
+            grayValue /= image.bytes.length / 4 * 3;
+          }else{
+            console.error('Unknown image pixel type');
+            return;
+          }
+          console.log('gray cal cost '+(Date.now()-timeStart)+'ms');
+
+          console.log('grayValue: '+grayValue);
+          if(grayValue < 20){ // 0~255, pls have a test
+            console.log('Try auto turn on torch');
+            try{
+              await cameraEnhancer.turnOnTorch();
+              bTorch = true;
+            }catch(ex){
+              console.error(ex);
+              console.warn('torch is not supported.');
+              bNotSupportTorch = true;
+              (ui!.shadowRoot!.querySelector('.mydbr-switch-torch') as HTMLElement).style.display = 'none';
+            }
+          }
+        }, 2000);
+
+      }catch(ex){
+        
+        if((ex as Error)?.message === strErrorDistoryed){
+          console.log(strErrorDistoryed);
+        }else{
+          console.error(ex);
         }
-        if(pRouter){
-          let router = await pRouter;
-          router.dispose();
-        }
-        if(cameraEnhancer){
-          cameraEnhancer.dispose();
-        }
-      })()};
-    },[]);
+      }
+
+      // distroy function will wait pInit
+      resolveInit!();
+    })();
+
+    // destory function
+    return ()=>{
+      bDestoryed = true;
+      (async()=>{
+        await pInit;
+        cvr?.dispose();
+        cameraEnhancer?.dispose();
+        if(null !== taskDetectDrak){ clearInterval(taskDetectDrak); }
+      })();
+    }
+  },[rfElContainer]);
   
-    return (
-      <button id="btn-scan" onClick={handleScan}>
-        Scan
-      </button>
-    );
+  // warning: don't add any other elements to this div, 
+  // so the CameraView(not mounted by react) will not be break.
+  return <div ref={rfElContainer} style={{width:'100vw',height:'70vh'}}></div>;
 }
 
 export default VideoRecognizer;
